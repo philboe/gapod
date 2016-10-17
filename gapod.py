8af9d85163c0b5107a508ccaa3f0437d64d0ec29
Philipp Bösch <philipp.boesch@rapid.ch>
2016-10-17 17:14:09 +0200

    some parsing

--- gapod.py
+++ gapod.py
@@ -9,20 +9,37 @@
 
 import os
 import datetime 
+import requests
+import re
+import logging
+APODURLSTART='http://apod.nasa.gov/apod/ap'
+APODURLEND='.html'
+REGEX=re.compile(rb"image\/\d{4}\/\w*\.jpg")
 
-def getRunningDesktopSession:
-    desktopSession=if os.environ.get('DESKTOP_SESSION') else 'none'
+def getRunningDesktopSession():
+    desktopSession= os.environ.get('DESKTOP_SESSION')
     return desktopSession
     
-   
+def extractImageNames(date):
+    pattern=re.compile(r"image\/\d{4}\/\w*\.jpg")
+    url=APODURLSTART+str(int(date)-2)+APODURLEND
+    logging.warning("opening url"+url)
+    website=requests.get(url)
+    html=website.text
+    imgNames=pattern.findall(html)
+    return imgNames
 
-def getRandomPastDate:
+def getRandomPastDate():
     """returns a random date from the past year"""
     return
 
-def getCurrentDate:
+def getCurrentDate():
     """Returns current date as YYMMDD string"""
     currentDate=datetime.datetime.now().strftime('%y%m%d')
     return currentDate
 
-
+if __name__ == '__main__':
+    logging.warning('started')
+    names=extractImageNames(getCurrentDate())    
+    print(names)
+    
